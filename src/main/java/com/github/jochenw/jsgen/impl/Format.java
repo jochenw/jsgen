3f11a8c3c4eaa5377e097ee966bb54d6df799cdd
Jochen Wiedmann (jwi) <jochen.wiedmann@softwareag.com>
2018-12-12 22:27:08 +0100

    Work on the Javadocs.

--- src/main/java/com/github/jochenw/jsgen/impl/Format.java
+++ src/main/java/com/github/jochenw/jsgen/impl/Format.java
@@ -70,30 +70,159 @@ public abstract class Format {
 	 *   annotations. The default implementation is {@link #NEWLINE}.
 	 */
 	public abstract Object getAnnotationSetSuffix();
+	/** Returns the prefix, which is used for introduction of an annotations attributes.
+	 * The default implementation is "(".
+	 * @return The prefix, which is used for introduction of an annotations attributes.
+	 * The default implementation is "(".
+	 * @see #getAnnotationValuesSuffix()
+	 */
 	public abstract Object getAnnotationValuesPrefix();
+	/** Returns the separator for an annotations attributes.
+	 * The default implementation is ", ".
+	 * @return The separator for an annotations attributes.
+	 * The default implementation is ", ".
+	 */
 	public abstract Object getAnnotationValueSeparator();
+	/** Returns the separator for an annotation attributes name, and value.
+	 * The default implementation is "=".
+	 * @return The separator for an annotation attributes name, and value.
+	 * The default implementation is "=".
+	 */
 	public abstract Object getAnnotationValueAssignment();
+	/** Returns the suffix, which is used for terminatimg an annotations attributes.
+	 * The default implementation is "(".
+	 * @return The suffix, which is used for terminating an annotations attributes.
+	 * The default implementation is "(".
+	 * @see #getAnnotationValuesPrefix()
+	 */
 	public abstract Object getAnnotationValuesSuffix();
+	/** Returns the sequence for terminating an anonymous code block.
+	 * This sequence shoulf include, in particular, the values "}",
+	 * {@link #NEWLINE}, and {@link #DEC_INDENT}, in a format specific
+	 * order. The default implementation is
+	 * {@link #DEC_INDENT}, {@link #INDENT}, "}", {@link #NEWLINE}.
+	 * 
+	 * @return The sequence for terminating an anonymous code block.
+	 */
 	public abstract Object getBlockTerminator();
+	/** Returns the sequence for starting a classes code block, This sequence will follow
+	 * the declaration of the classes properties, like "public class Foo extends Bar", etc.
+	 * The default implementation is " ", "{", {@link #INC_INDENT}, {@link #NEWLINE}
+	 * @return The sequence for starting a classes code block.
+	 */
 	public abstract Object getClassBlockHeader();
+	/** Returns the sequence for terminating a classes code block. This sequence will
+	 * follow the declrataions of all the classes members, effectively closing the class
+	 * specification. The default implementation is {@link #DEC_INDENT},
+	 * {@link #INDENT}, "}", {@link #NEWLINE}.
+	 * @return The sequence for terminating a classes code block.
+	 */
 	public abstract Object getClassBlockFooter();
+	/** Returns the prefix for a classes comment. The default implementation is
+	 * {@link #NOTHING}.
+	 * @return The prefix for a classes comment. The default implementation is
+	 * {@link #NOTHING}.
+	 */
 	public abstract Object getClassCommentPrefix();
+	/** Returns the suffix for a classes comment. The default implementation is
+	 * {@link #NOTHING}.
+	 * @return The suffix for a classes comment. The default implementation is
+	 * {@link #INDENT}.
+	 */
 	public abstract Object getClassCommentSuffix();
+	/** Returns the prefix of a private comment. The default implementation is
+	 * {@link #INDENT}, "/*".
+	 * @return The prefix of a private comment. The default implementation is
+	 * {@link #INDENT}, "/*".
+	 */
 	public abstract Object getCommentPrivatePrefix();
+	/** Returns the separator for two lines of a private comment. The default
+	 * implementation is {@link #NEWLINE}, {@link #INDENT}, "  * ".
+	 * @return The separator for two lines of a private comment. The default
+	 * implementation is {@link #NEWLINE}, {@link #INDENT}, "  * ".
+	 */
 	public abstract Object getCommentPrivateSeparator();
+	/** Returns the suffix of a private comment. The default implementation is
+	 * {@link #NEWLINE}, {@link #INDENT}, {@literal " &ast;/"}, {@link #NEWLINE}.
+	 * @return The suffix of a private comment. The default implementation is
+     * {@link #NEWLINE}, {@link #INDENT}, {@literal " &ast;/"}, {@link #NEWLINE}.
+	 */
 	public abstract Object getCommentPrivateSuffix();
+	/** Returns the prefix of a public comment. The default implementation is
+	 * {@link #INDENT}, "/**".
+	 * @return The prefix of a public comment. The default implementation is
+	 * {@link #INDENT}, "/**".
+	 */
 	public abstract Object getCommentPublicPrefix();
+	/** Returns the separator for two lines of a public comment. The default
+	 * implementation is {@link #NEWLINE}, {@link #INDENT}, "   * ".
+	 * @return The separator for two lines of a public comment. The default
+	 * implementation is {@link #NEWLINE}, {@link #INDENT}, "   * ".
+	 */
 	public abstract Object getCommentPublicSeparator();
+	/** Returns the suffix of a public comment. The default implementation is
+	 * {@link #NEWLINE}, {@link #INDENT}, {@literal "  &ast;/"}, {@link #NEWLINE}.
+	 * @return The suffix of a public comment. The default implementation is
+     * {@link #NEWLINE}, {@link #INDENT}, {@literal "  &ast;/"}, {@link #NEWLINE}.
+	 */
 	public abstract Object getCommentPublicSuffix();
+	/** Returns a single line comments prefix, default implementation is
+	 * {@link #INDENT}, "// ".
+	 * @return A single line comments prefix, default implementation is
+	 * {@link #INDENT}, "// ".
+	 */
 	public abstract Object getCommentSingleLinePrefix();
+	/** Returns a single line comments suffix, default implementation is
+	 * {@link #NEWLINE}, "// ".
+	 * @return A single line comments suffix. The default implementation is
+	 * {@link #NEWLINE}, "// ".
+	 */
 	public abstract Object getCommentSingleLineSuffix();
+	/** Returns a do ... while ... blocks header, default implementation
+	 * is {@link #INDENT}, "do {", {@link #NEWLINE}, {@link #INC_INDENT}
+	 * @return A do ... while ... blocks header. The default implementation
+	 * is {@link #INDENT}, "do {", {@link #NEWLINE}, {@link #INC_INDENT}
+	 */
 	public abstract Object getDoWhileBlockHeader();
+	/** Returns a do ... while ... blocks terminator, effectively the
+	 * separator of the code block, and the condition. The default implementation
+	 * is {@link #DEC_INDENT}, {@link #INDENT}, "while (".
+	 * @return A do ... while ... blocks terminator, effectively the
+	 * separator of the code block, and the condition. The default implementation
+	 * is {@link #DEC_INDENT}, {@link #INDENT}, "while (".
+	 */
 	public abstract Object getDoWhileBlockTerminator();
+	/** Returns the terminator of a do ... while ... blocks condition.
+	 *The default implementation is ")", {@link #NEWLINE}.
+	 * @return The terminator of a do ... while ... blocks condition.
+	 *The default implementation is ")", {@link #NEWLINE}.
+	 */
 	public abstract Object getDoWhileTerminator();
+	/** Returns the prefix of a field declaration (default {@link #INDENT}).
+	 * @return The prefix of a field declaration (default {@link #INDENT}).
+	 */
 	public abstract Object getFieldPrefix();
+	/** Returns the sequence for starting a fields value assignment
+	 * (default " = ").
+	 * @return The sequence for starting a fields value assignment
+	 * (default " = ").
+	 */
 	public abstract Object getFieldValueAssignment();
+	/** Returns the suffix of a field declaration (default ";", {@link #NEWLINE}).
+	 * @return The suffix of a field declaration (default ";", {@link #NEWLINE}).
+	 */
 	public abstract Object getFieldSuffix();
+	/** Returns the prefix of a for ... blocks condition (default
+	 * {@ljnk #INDENT}, "for (").
+	 * @return The prefix of a for ... blocks condition (default
+	 * {@ljnk #INDENT}, "for (").
+	 */
 	public abstract Object getForConditionPrefix();
+	/** Returns the prefix of a for ... blocks condition (default
+	 * {@ljnk #INDENT}, "for (").
+	 * @return The prefix of a for ... blocks condition (default
+	 * {@ljnk #INDENT}, "for (").
+	 */
 	public abstract Object getForConditionSuffix();
 	public abstract Object getIfConditionPrefix();
 	public abstract Object getIfConditionSuffix();
